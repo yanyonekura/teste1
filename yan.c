454ae8996ba437ddb189deaaa8595fcb56a937fc
yanyonekura <yanyonekura2007@gmail.com>
2025-07-18 15:00:26 -0300

    Update yan.c

--- yan.c
+++ yan.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main(){
-    int n;
-    printf("oi");
+
+int main() {
+    int a, negativos = 0;
+
+    for(int i = 1; i <= 5; i++) {
+        printf("Digite o valor %d: ", i);
+        scanf("%d", &a);
+        if(a < 0) {
+            negativos++;
+        }
+    }
+
+    printf("Quantidade de valores negativos: %d\n", negativos);
     return 0;
 }
